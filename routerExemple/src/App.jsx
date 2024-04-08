df5738d3a1d1d614c254c02fdec8f91d00e83f78
Brasero <brandonricci4@gmail.com>
2024-04-08 10:11:58 +0200

    presentation router

--- routerExemple/src/App.jsx
+++ routerExemple/src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css'
+import {Routes, Route, NavLink} from "react-router-dom"
+import Page1 from "./pages/Page1/index.jsx";
+import Page2 from "./pages/Page2/index.jsx";
 
 function App() {
 
   return (
     <>
+      <nav>
+        <NavLink style={({isActive}) => {
+          return isActive ? {color: 'orange'} : {}
+        }} to={'/'} >Page1</NavLink>
+        <NavLink to={'/page2'} >Page2</NavLink>
+      </nav>
+      <Routes>
+        <Route path={"/"} element={<Page1 />} />
+        <Route path={'/page2'} element={<Page2 />} />
+      </Routes>
     </>
   )
 }
